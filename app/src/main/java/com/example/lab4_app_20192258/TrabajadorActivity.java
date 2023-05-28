5257acec68d90bbbac7102ff0712f26e322ac0f5
angelo <a20192258@pucp.edu.pe>
2023-05-28 12:06:23 -0500

    Descargar Imagen

--- app/src/main/java/com/example/lab4_app_20192258/TrabajadorActivity.java
+++ app/src/main/java/com/example/lab4_app_20192258/TrabajadorActivity.java
@@ -1,13 +1,25 @@
 package com.example.lab4_app_20192258;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 
 import com.example.lab4_app_20192258.databinding.ActivityMainBinding;
 import com.example.lab4_app_20192258.databinding.ActivityTrabajadorBinding;
+
+import java.io.File;
 
 public class TrabajadorActivity extends AppCompatActivity {
     ActivityTrabajadorBinding binding;
@@ -24,5 +36,56 @@ public class TrabajadorActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                descargarConDownloadManager();
+            }
+        });
+
     }
+
+
+    public void descargarConDownloadManager() {
+
+        String permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE; //si no funciona android.Manifest.permission…
+
+        // >29
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ||
+                ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+            //si tengo permisos
+            String fileName = "horarios.jpg";
+            String endPoint = "https://i.pinimg.com/564x/4e/8e/a5/4e8ea537c896aa277e6449bdca6c45da.jpg";
+
+            Uri downloadUri = Uri.parse(endPoint);
+            DownloadManager.Request request = new DownloadManager.Request(downloadUri);
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+            request.setAllowedOverRoaming(false);
+            request.setTitle(fileName);
+            request.setMimeType("image/jpeg");
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, File.separator + fileName);
+
+            DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+            dm.enqueue(request);
+        } else {
+            //si no tiene permisos
+            launcher.launch(permission);
+        }
+    }
+
+    ActivityResultLauncher<String> launcher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            isGranted -> {
+
+                if (isGranted) { // permiso concedido
+                    descargarConDownloadManager();
+                } else {
+                    Log.e("msg-test", "Permiso denegado");
+                }
+            });
+
+
+
 }
