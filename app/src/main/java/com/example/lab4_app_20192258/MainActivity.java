a821d0ee0f55c75141e296b07c790c50f56d413b
angelo <a20192258@pucp.edu.pe>
2023-05-27 22:18:22 -0500

    Primer commit

---
+++ app/src/main/java/com/example/lab4_app_20192258/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.lab4_app_20192258;
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.lab4_app_20192258.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    String channel1Id = "canal1";
+    String channel2Id = "canal2";
+    public static String TAG = "msg-test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        createNotificationChannel();
+
+        binding.button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, TutorActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel1Id)
+                    .setSmallIcon(R.drawable.baseline_auto_stories_24)
+                    .setContentTitle("Modo Tutor")
+                    .setContentText("“Está entrando en modo Tutor")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            Notification notification = builder.build();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                notificationManager.notify(1, notification);
+            }
+        });
+
+        binding.button2.setOnClickListener(view -> {
+            Intent intent = new Intent(this, TrabajadorActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel1Id)
+                    .setSmallIcon(R.drawable.baseline_auto_stories_24)
+                    .setContentTitle("Modo Empleado")
+                    .setContentText("Está entrando en modo Empleado")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            Notification notification = builder.build();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                notificationManager.notify(2, notification);
+            }
+        });
+    }
+
+    public void createNotificationChannel() {
+        //android.os.Build.VERSION_CODES.O == 26
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channel1Id,
+                    "Canal 1 notificaciones",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Este es para los mensajes nuevos");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            NotificationChannel channel2 = new NotificationChannel(channel2Id,
+                    "Canal 1 notificaciones",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Este es para los mensajes que no llegaron a enviarse");
+            notificationManager.createNotificationChannel(channel2);
+
+            askPermission();
+
+        }
+    }
+
+    public void askPermission(){
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{POST_NOTIFICATIONS}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 101){
+            Log.d(TAG,"permiso concedido");
+        }else{
+            Log.d(TAG,"permiso denegado");
+        }
+    }
+}
