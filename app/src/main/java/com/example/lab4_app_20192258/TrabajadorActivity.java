6389c68e3810dea3d981df62cd65ac5863f52eae
angelo <a20192258@pucp.edu.pe>
2023-05-28 12:27:38 -0500

    Descargar Imagen + mini validacion

--- app/src/main/java/com/example/lab4_app_20192258/TrabajadorActivity.java
+++ app/src/main/java/com/example/lab4_app_20192258/TrabajadorActivity.java
@@ -13,13 +13,27 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.lab4_app_20192258.Retrofit.EmployeeRepository;
 import com.example.lab4_app_20192258.databinding.ActivityMainBinding;
 import com.example.lab4_app_20192258.databinding.ActivityTrabajadorBinding;
+import com.example.lab4_app_20192258.entity.Employee;
+import com.example.lab4_app_20192258.entity.EmployeeDTO;
 
 import java.io.File;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TrabajadorActivity extends AppCompatActivity {
     ActivityTrabajadorBinding binding;
@@ -37,13 +51,70 @@ public class TrabajadorActivity extends AppCompatActivity {
             }
         });
 
-        binding.button4.setOnClickListener(new View.OnClickListener() {
+
+
+
+        EditText inputText = binding.editTextTextPersonName2;
+        if (inputText.getText().toString().isEmpty()) {
+            // Si está vacío, desactivar el botón
+            Button button = binding.button4;
+            button.setEnabled(false);
+        }
+
+        inputText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                descargarConDownloadManager();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (inputText.getText().toString().isEmpty()) {
+                    // Si está vacío, desactivar el botón
+                    Button button = binding.button4;
+                    button.setEnabled(false);
+                }else{
+                    // Si no está vacío, activar el botón
+                    Button button = binding.button4;
+                    button.setEnabled(true);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EmployeeRepository employeeRepository = new Retrofit.Builder()
+                                    .baseUrl("http://192.168.100.108:8080")
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                    .build().create(EmployeeRepository.class);
+                            employeeRepository.obtenerEmployee(Integer.parseInt(inputText.getText().toString())).enqueue(new Callback<EmployeeDTO>() {
+                                @Override
+                                public void onResponse(Call<EmployeeDTO> call, Response<EmployeeDTO> response) {
+                                    if (response.isSuccessful()) {
+                                        Employee employee = response.body().getEmployee();
+                                        if((int)employee.getMeeting() == 1){
+                                            descargarConDownloadManager();
+                                            Toast.makeText(getApplicationContext(),"Descargando horarios", Toast.LENGTH_SHORT).show();
+                                        }else{
+                                            Toast.makeText(getApplicationContext(),"No cuenta con tutorías pendientes", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Log.d("msg-test", "error en la respuesta del webservice");
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<EmployeeDTO> call, Throwable t) {
+                                    t.printStackTrace();
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
-
     }
 
 
